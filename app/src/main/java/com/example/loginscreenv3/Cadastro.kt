3220dc89751b2df5782f9270e77bbdab8cd6ac75
Isaac_Durand <isaacdurand@hotmail.com>
2024-09-25 11:56:32 -0300

    Initial commit

---
+++ app/src/main/java/com/example/loginscreenv3/Cadastro.kt
@@ -0,0 +1,124 @@
+package com.example.loginscreenv3
+
+import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.example.loginscreenv3.ui.theme.LoginScreenV3Theme
+import org.w3c.dom.Text
+
+//class Cadastro : ComponentActivity() {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        enableEdgeToEdge()
+//        setContent {
+//            LoginScreenV3Theme {
+//                CadastroScreen()
+//            }
+//        }
+//    }
+//}
+
+
+@Composable
+fun CadastroScreen(navController: NavController){
+
+    var primeiroNome by remember {mutableStateOf("")}
+    var ultimoNome by remember {mutableStateOf("")}
+    var apelido by remember {mutableStateOf("")}
+    var email by remember {mutableStateOf("")}
+    var altura by remember {mutableStateOf("")}
+    var peso by remember {mutableStateOf("")}
+    var idade by remember {mutableStateOf("")}
+    var senha by remember {mutableStateOf("")}
+    var senhaConfirm by remember {mutableStateOf("")}
+
+    Column(modifier = Modifier
+        .padding(16.dp)
+        .fillMaxSize(),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.Start){
+        Box(modifier = Modifier.padding(10.dp),){
+            Text("Cadastro:", fontSize = 30.sp)
+        }
+
+        Column(modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp)) {
+            OutlinedTextField(value = email,onValueChange = {email = it},label ={Text("E-mail")},modifier = Modifier.fillMaxWidth())
+            OutlinedTextField(value = senha,onValueChange ={senha = it},label ={Text("Senha")},modifier = Modifier.fillMaxWidth())
+            OutlinedTextField(value =senhaConfirm,onValueChange ={senhaConfirm=it},label ={Text("Confirma senha")},modifier = Modifier.fillMaxWidth())
+            OutlinedTextField(value = primeiroNome,onValueChange = {primeiroNome = it},label = {Text("Primeiro nome")},modifier = Modifier.fillMaxWidth())
+            OutlinedTextField(value = ultimoNome,onValueChange = {ultimoNome = it},label ={Text("Ultimo nome")},modifier = Modifier.fillMaxWidth())
+        }
+
+        Row(modifier = Modifier.fillMaxWidth(),horizontalArrangement = Arrangement.SpaceBetween) {
+            OutlinedTextField(value = apelido, onValueChange = {apelido = it}, label ={Text("Apelido")}, modifier = Modifier
+                .weight(1f)
+                .fillMaxWidth()
+                .padding(10.dp))
+            OutlinedTextField(value = altura, onValueChange = {altura = it}, label ={ Text("Altura") }, modifier = Modifier
+                .weight(1f)
+                .fillMaxWidth()
+                .padding(10.dp),keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number))
+        }
+
+        Row(modifier = Modifier.fillMaxWidth(),horizontalArrangement = Arrangement.SpaceBetween) {
+            OutlinedTextField(value = peso, onValueChange = {peso = it}, label ={ Text("Peso") }, modifier = Modifier
+                .weight(1f)
+                .fillMaxWidth()
+                .padding(10.dp),keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number))
+            OutlinedTextField(value = idade, onValueChange = {idade = it}, label ={ Text("Idade") }, modifier = Modifier
+                .weight(1f)
+                .fillMaxWidth()
+                .padding(10.dp),keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number))
+        }
+
+        Row(modifier = Modifier.fillMaxWidth().padding(0.dp,220.dp,0.dp,0.dp), verticalAlignment = Alignment.Bottom){
+            Box{Button(modifier = Modifier.fillMaxWidth(),onClick = {navController.popBackStack()}){Text("Cadastrar")}}
+        }
+    }
+
+
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun CasdastroScreenPreview() {
+    CadastroScreen(rememberNavController())
+}
---
+++ app/src/main/java/com/example/loginscreenv3/Login.kt
@@ -0,0 +1,171 @@
+package com.example.loginscreenv3
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.NavHost
+import androidx.navigation.compose.rememberNavController
+import com.example.loginscreenv3.ui.theme.LoginScreenV3Theme
+
+@Composable
+fun LoginScreen(navController: NavController) {
+
+    val context = LocalContext.current
+
+    val usuario = "exemplo@gmail.com"
+    val senha = "123456"
+
+    var textFieldUsuario by remember { mutableStateOf("") }
+    var textFieldSenha by remember { mutableStateOf("") }
+    var check by remember{ mutableStateOf(false) }
+
+    Column(
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                "\"App\"",
+                fontSize = 32.sp,
+                fontWeight = FontWeight.Bold,
+            )
+        }
+
+        Spacer(modifier = Modifier.height(66.dp))
+
+        Box(modifier = Modifier
+            .fillMaxWidth()
+            .padding()) {
+            OutlinedTextField(
+                value = textFieldUsuario,
+                onValueChange = { textFieldUsuario = it },
+                label = { Text("Usuário") },
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
+
+        Spacer(modifier = Modifier.height(12.dp))
+
+        Box(modifier = Modifier
+            .fillMaxWidth()
+            .padding()) {
+            OutlinedTextField(
+                value = textFieldSenha,
+                onValueChange = { textFieldSenha = it },
+                label = { Text("Senha") },
+                modifier = Modifier.fillMaxWidth(),
+                visualTransformation = PasswordVisualTransformation()
+            )
+        }
+
+        Row(modifier = Modifier
+            .padding()
+            .fillMaxWidth(), horizontalArrangement = Arrangement.Start, verticalAlignment = Alignment.CenterVertically){
+            Box(modifier = Modifier.padding()){
+                Checkbox(checked = check, onCheckedChange ={check = it} )
+            }
+            Box(modifier = Modifier.padding()){
+                Text(text = "Lembrar usuário", fontWeight = FontWeight.Medium)
+            }
+        }
+
+
+        Spacer(modifier = Modifier.height(38.dp))
+
+        Row(modifier = Modifier.padding(10.dp),verticalAlignment = Alignment.CenterVertically) {
+            //Box(modifier = Modifier
+            //    .fillMaxWidth()
+            //    .padding(10.dp),
+            //    contentAlignment = Alignment.Center) {
+            Button(modifier = Modifier.padding(10.dp), onClick = {
+
+                if (textFieldUsuario != usuario) {
+                    Toast.makeText(context, "Usuario incorreto.", Toast.LENGTH_SHORT).show()
+                    return@Button
+                }
+
+                if (textFieldSenha != senha) {
+                    Toast.makeText(context, "Senha Incorreta", Toast.LENGTH_SHORT).show()
+                    return@Button
+                }
+
+                Toast.makeText(context, "Login bem sucedido.", Toast.LENGTH_SHORT).show()
+
+
+//                if (textFieldUsuario==usuario){
+//                    //Toast.makeText(context, "Usuario correto.", Toast.LENGTH_SHORT).show()
+//                    if (textFieldSenha == senha) {
+//                        Toast.makeText(context, "Login bem sucedido.", Toast.LENGTH_SHORT).show()
+//                    } else {
+//                        Toast.makeText(context, "Login falhou.", Toast.LENGTH_SHORT).show()
+//                    }
+//                }else{
+//                    Toast.makeText(context, "Usuario incorreto.", Toast.LENGTH_SHORT).show()
+//                }
+
+            }) {
+                Text(text = "Entrar")
+            }
+            //}
+
+            //Box(modifier = Modifier
+            //    .fillMaxWidth()
+            //    .padding(10.dp),
+            //    contentAlignment = Alignment.Center){
+            Button(modifier = Modifier.padding(10.dp),onClick = {
+                val isClicked = true
+                if (isClicked) {
+                    navController.navigate("CadastroScreen")
+                }
+
+            }) {
+                Text(text = "Cadastrar-se")
+            }
+            //}
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun LoginScreenPreview() {
+    LoginScreen(rememberNavController())
+}
